d5e70b8e638027dcbb50e73cb8e5c6cde996624a
Zichao LI <cszichao@gmail.com>
2023-04-08 22:08:33 +0800

    added new str num util

--- num.go
+++ num.go
@@ -2,6 +2,7 @@ package strnum
 
 import (
 	"encoding/json"
+	"strconv"
 )
 
 type StrNum struct {
@@ -22,4 +23,16 @@ func (s StrNum) MarshalJSON() ([]byte, error) {
 func (s StrNum) Val() int64 {
 	v, _ := s.Number.Int64()
 	return v
+}
+
+func NewStr(s string) StrNum {
+	return StrNum{json.Number(s)}
+}
+
+func NewInt64(i int64) StrNum {
+	return StrNum{json.Number(strconv.FormatInt(i, 10))}
+}
+
+func NewInt(i int) StrNum {
+	return StrNum{json.Number(strconv.Itoa(i))}
 }
